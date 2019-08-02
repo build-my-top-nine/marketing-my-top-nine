309359d9b52f7ffe25e7f442b3c04c5fe62cbab0
Ali Dervisoglu <derya.dervisoglu@gmail.com>
2019-08-01 17:42:54 -0700

    aboutus page done

---
+++ js/aboutus.js
@@ -0,0 +1,21 @@
+const menuButtonSelector = document.querySelector('.mobile-header');
+
+const mobileBodySelector = document.querySelector('.mobilemove');
+const dropDownSelector = document.querySelector('.dropdown');
+const dropDownItemSelector = document.querySelectorAll('.dropdown-item')
+
+function menuVisibility () {
+    document.querySelector('.mobile-header').classList.toggle("show")
+}
+
+menuButtonSelector.addEventListener('click', () => {
+    console.log('clicked menu')
+    menuButtonSelector.classList.toggle("drop-body2")
+    mobileBodySelector.classList.toggle("drop-body")
+    dropDownSelector.classList.toggle("dropdown-down");
+    dropDownItemSelector[0].classList.toggle("dropdown-items-down");
+    dropDownItemSelector[1].classList.toggle("dropdown-items-down");
+    dropDownItemSelector[2].classList.toggle("dropdown-items-down");
+    dropDownItemSelector[3].classList.toggle("dropdown-items-down");
+    event.stopPropagation();
+}, );
